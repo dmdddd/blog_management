7be3dcc80ae8665f9867b39fb9edda81704f59a3
Daniel <dk1989@gmail.com>
2024-09-16 09:50:56 -0500

    Using mongodb for upvotes and comments

--- my-blog/src/pages/ArticlePage.js
+++ my-blog/src/pages/ArticlePage.js
@@ -1,11 +1,42 @@
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import axios from 'axios';
 import articles from "./article-content";
 import NotFoundPage from "./NotFoundPage";
+import CommentsList from "../components/CommentsList";
+import AddCommentForm from "../components/AddCommentForm";
 
 const ArticlePage = () => {
+    // Adding a state "ArticleInfo" to the "ArticlePageComponent"
+    // To set the state, we user setArticleInfo function
+    // Currently initialized with { upvotes: 0, comments: [] }
+    const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
+
+
+    // useState -> adds memory
+    // useEffect -> updates it
+    // Adding logic to our components, executed outside of the normal rendering
+
+    useEffect(() => {
+        // Define a function to retrieve the data
+        const loadArticleInfo = async () => {
+            const response = await axios.get(`/api/articles/${articleId}`);
+            const newArticleInfo = response.data;
+            setArticleInfo(newArticleInfo);
+        }
+        // Use the defined function
+        loadArticleInfo();
+    }, []);
+
     const params = useParams();
     const articleId = params.articleId;
-    const article = articles.find(article => article.name == articleId)
+    const article = articles.find(article => article.name === articleId)
+
+    const addUpvote = async () => {
+        const response = await axios.put(`/api/articles/${articleId}/upvote`);
+        const updatedArticle = response.data;
+        setArticleInfo(updatedArticle);
+    }
 
     if (!article){
         return <NotFoundPage />
@@ -14,12 +45,19 @@ const ArticlePage = () => {
     return (
         <>
         <h1>{article.title}</h1>
+        <div className="upvotes-section">
+            <button onClick={addUpvote}>Upvote</button>
+            <p>This article has {articleInfo.upvotes} upvote(s)</p>
+        </div>
         {article.content.map((paragraph, i) => (
             <p key={i}>{paragraph}</p>
         ))}
+        <AddCommentForm
+            articleName={articleId}
+            onArticleupdated={updatedArticle => setArticleInfo(updatedArticle)} />
+        <CommentsList comments={articleInfo.comments}/>
         </>
-        
     );
 }
 
-export default ArticlePage;
+export default ArticlePage; 
