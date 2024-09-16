c6bc53a74726302092b2988546a74c086b74eb01
Daniel <dk1989@gmail.com>
2024-09-16 09:51:23 -0500

    Creating a comment addition form component

---
+++ my-blog/src/components/AddCommentForm.js
@@ -0,0 +1,40 @@
+import { useState, useEffect } from "react"
+import axios from 'axios';
+
+const AddCommentForm = ({ articleName, onArticleupdated }) => {
+    const [name, setName] = useState('');
+    const [commentText, setCommentText] = useState('');
+
+    const addComment = async () => {
+        const response = await axios.post(`/api/articles/${articleName}/comments`, {
+            postedBy: name,
+            text:commentText,
+        });
+        const updatedArticle = response.data;
+        onArticleupdated(updatedArticle);
+        setName('');
+        setCommentText('');
+    }
+    return (
+        <div id="add-comment-form">
+            <h3>Add a Comment</h3>
+            <label>
+                Name:
+                <input
+                    value={name}
+                    onChange={e => setName(e.target.value)}
+                    type="text" />
+            </label>
+            <label>
+                Comment:
+                <textarea
+                    value={commentText}
+                    onChange={e => setCommentText(e.target.value)}
+                    rows="4" cols="50" />
+            </label>
+            <button onClick={addComment}>Add Comment</button>
+        </div>
+    )
+}
+
+export default AddCommentForm
