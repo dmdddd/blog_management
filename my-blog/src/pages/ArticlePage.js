293cde720718293b0d729c7b332469bc9e53dad7
Daniel <dk1989@gmail.com>
2024-09-30 14:50:28 -0500

    Check if article exists, show error message

--- my-blog/src/pages/ArticlePage.js
+++ my-blog/src/pages/ArticlePage.js
@@ -1,8 +1,6 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from 'axios';
-import articles from "./article-content";
-import NotFoundPage from "./NotFoundPage";
 import CommentsList from "../components/CommentsList";
 import AddCommentForm from "../components/AddCommentForm";
 import useUser from "../hooks/useUser";
@@ -11,7 +9,9 @@ const ArticlePage = () => {
     // Adding a state "ArticleInfo" to the "ArticlePageComponent"
     // To set the state, we user setArticleInfo function
     // Currently initialized with { upvotes: 0, comments: [] }
-    const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
+    const [articleInfo, setArticleInfo] = useState({ upvotes: 0, content: [], comments: [], canUpvote: false });
+    const {canUpvote} = articleInfo;
+    const [articleFound, setArticleFound] = useState(false);
 
     // useState -> adds memory
     // useEffect -> updates it
@@ -22,50 +22,68 @@ const ArticlePage = () => {
     useEffect(() => {
         // Define a function to retrieve the data
         const loadArticleInfo = async () => {
-            const response = await axios.get(`/api/articles/${articleId}`);
-            const newArticleInfo = response.data;
-            setArticleInfo(newArticleInfo);
+            const token = user && await user.getIdToken();
+            const headers = token ? { authtoken: token } : {};
+            try {
+                const response = await axios.get(`/api/articles/${articleId}`, { headers });
+                const newArticleInfo = response.data;
+                setArticleFound(true);
+                setArticleInfo(newArticleInfo);
+            } catch (e) {
+                console.log("Article '" + articleId + "' not found")
+            }
         }
-        // Use the defined function
-        loadArticleInfo();
-    }, []);
+        if (!isLoading) {
+            loadArticleInfo();
+        }
+    }, [isLoading, user]);
 
     const params = useParams();
     const articleId = params.articleId;
-    const article = articles.find(article => article.name === articleId)
 
     const addUpvote = async () => {
-        const response = await axios.put(`/api/articles/${articleId}/upvote`);
+        const token = user && await user.getIdToken();
+        const headers = token ? { authtoken: token } : {};
+        const response = await axios.put(`/api/articles/${articleId}/upvote`, null, { headers });
         const updatedArticle = response.data;
         setArticleInfo(updatedArticle);
     }
 
-    if (!article){
-        return <NotFoundPage />
+    if (articleFound)
+    {
+        // Atricle found
+        return (
+            <>
+            <h1>{articleInfo.title}</h1>
+            <div className="upvotes-section">
+                { user
+                    ? <button onClick={addUpvote}>{canUpvote ? 'Upvote' : 'Already Upvoted'}</button>
+                    : <button>Log in to upvote</button>
+                    }
+                <p>This article has {articleInfo.upvotes} upvote(s)</p>
+            </div>
+            {articleInfo.content.map((paragraph, i) => (
+                <p key={i}>{paragraph}</p>
+            ))}
+            { user
+                ? <AddCommentForm
+                    articleName={articleId}
+                    onArticleupdated={updatedArticle => setArticleInfo(updatedArticle)} />
+                : <button>Log in to add a comment</button>
+            }
+            <CommentsList comments={articleInfo.comments}/>
+            </>
+        );
+    } else {
+        // Article not found
+        return (
+            <>
+             <h1>Article not found</h1>
+             <p>Requested article: {articleId}</p>
+            </>
+        )
     }
     
-    return (
-        <>
-        <h1>{article.title}</h1>
-        <div className="upvotes-section">
-            { user
-                ? <button onClick={addUpvote}>Upvote</button>
-                : <button>Log in to upvote</button>
-                }
-            <p>This article has {articleInfo.upvotes} upvote(s)</p>
-        </div>
-        {article.content.map((paragraph, i) => (
-            <p key={i}>{paragraph}</p>
-        ))}
-        { user
-            ? <AddCommentForm
-                articleName={articleId}
-                onArticleupdated={updatedArticle => setArticleInfo(updatedArticle)} />
-            : <button>Log in to add a comment</button>
-        }
-        <CommentsList comments={articleInfo.comments}/>
-        </>
-    );
 }
 
 export default ArticlePage; 
