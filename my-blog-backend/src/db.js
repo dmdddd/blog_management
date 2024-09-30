6cf658bac040b4e451dc4924acafa6b1477c960b
Daniel <dk1989@gmail.com>
2024-09-30 14:50:00 -0500

    Connect to mongoDB database

--- my-blog-backend/src/db.js
+++ my-blog-backend/src/db.js
@@ -3,9 +3,11 @@ import { MongoClient } from 'mongodb';
 let db;
 
 async function connectToDb(cb){
-    const client = new MongoClient('mongodb://127.0.0.1:27017');
+    // const client = new MongoClient('mongodb://127.0.0.1:27017'); // local
+    const client = new MongoClient(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.esbpl.mongodb.net/`);
     await client.connect();
-    db = client.db('react-blog-db');
+    // db = client.db('react-blog-db'); // local
+    db = client.db('react_blog');
     cb();
 }
 
