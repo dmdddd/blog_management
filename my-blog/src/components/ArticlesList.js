630e40afd4c15585704a0a310cac46db1488120f
Daniel <dk1989@gmail.com>
2024-09-30 15:52:31 -0500

    Stop using the json for article data and move to the database

--- my-blog/src/components/ArticlesList.js
+++ my-blog/src/components/ArticlesList.js
@@ -5,7 +5,7 @@ const ArticlesList = ({articles}) => {
         <>
         {articles.map(article => (
             <Link key={article.name} className="article-list-item" to={`/articles/${article.name}`}>
-                <h3>{article.name}</h3>
+                <h3>{article.title}</h3>
                 <p>{article.content[0].substring(0, 150)}...</p>
             </Link>
         ))}
--- my-blog/src/pages/ArticlesListPage.js
+++ my-blog/src/pages/ArticlesListPage.js
@@ -1,6 +1,25 @@
-import articles from "./article-content";
+import { useState, useEffect } from "react"
 import ArticlesList from "../components/ArticlesList";
+import axios from 'axios';
+
 const ArticlesListPage = () => {
+    const [articles, setArticles] = useState([]);
+
+    useEffect(() => {
+        // Define a function to retrieve the data
+        const loadArticles = async () => {
+            try {
+                const response = await axios.get('/api/articles');
+                const articlesData = response.data;
+                setArticles(articlesData);
+                // console.log(articles);
+            } catch (e) {
+                console.log("Articles not found")
+            }
+        }
+        loadArticles();
+    }, []);
+    
     return (
         <>
         <h1>Articles</h1>
