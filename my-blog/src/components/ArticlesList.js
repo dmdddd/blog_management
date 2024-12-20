e6236768e1ed47b9a54df3aa91a00991a5928b05
dmdddd <dk198@gmail.com>
2024-12-20 17:36:28 -0600

    Removing html tags from description in article list page

--- my-blog/src/components/ArticlesList.js
+++ my-blog/src/components/ArticlesList.js
@@ -4,12 +4,17 @@ const ArticlesList = ({articles, blog}) => {
     if (!blog){
         return <p>Loading</p>
     }
+
+    const stripHtmlTags = (html) => {
+        return html.replace(/<[^>]*>/g, '');
+     }
+
     return (
         <>
         {articles.map(article => (
             <Link key={article.name} className="article-list-item" to={`/blogs/${article.blog}/articles/${article.name}`} state={{ blog }}>
                 <h3>{article.title}</h3>
-                <p>{article.content.substring(0, 150)}...</p>
+                <p>{stripHtmlTags(article.content.substring(0, 150))}...</p>
             </Link>
         ))}
         </>
