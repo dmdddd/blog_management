3e62d522c21b04cf6b251f4606c3bcd96a764877
dmdddd <dk1989@gmail.com>
2025-01-03 11:08:18 -0600

    Addign sortign to the blog list page

--- my-blog/src/pages/BlogsListPage.js
+++ my-blog/src/pages/BlogsListPage.js
@@ -18,7 +18,9 @@ const BlogsListPage = () => {
         totalItems: 0,
         hasNext: false,
         hasPrev: false,
-      });
+    });
+    const [sortBy, setSortBy] = useState('createdAt');
+    const [sortDir, setSortDir] = useState('asc');
 
     useEffect(() => {
         // Define a function to retrieve the data
@@ -26,7 +28,8 @@ const BlogsListPage = () => {
             try {
                 const token = user && await user.getIdToken();
                 const headers = token ? { authtoken: token } : {};
-                const response = await axios.get(`/api/blogs?page=${page}&size=${size}`, { headers });
+                console.log(`/api/blogs?page=${pagination.currentPage}&size=${pagination.pageSize}&sortBy=${sortBy}&sortDir=${sortDir}`);
+                const response = await axios.get(`/api/blogs?page=${pagination.currentPage}&size=${pagination.pageSize}&sortBy=${sortBy}&sortDir=${sortDir}`, { headers });
                 const blogsData = response.data.data;
                 setBlogs(blogsData);
                 setPagination(response.data.pagination);
@@ -37,17 +40,44 @@ const BlogsListPage = () => {
             }
         }
         if (!isLoading)
-            loadBlogs(pagination.currentPage, pagination.pageSize);
-    }, [isLoading, pagination.currentPage, pagination.pageSize]);
+            loadBlogs();
+    }, [isLoading, pagination.currentPage, pagination.pageSize, sortBy, sortDir]);
 
     const handlePageChange = (page) => {
         setPagination((prev) => ({ ...prev, currentPage: page }));
-      };
-    
-      const handlePageSizeChange = (size) => {
+    };
+
+    const handlePageSizeChange = (size) => {
         setPagination((prev) => ({ ...prev, pageSize: size, currentPage: 1 }));
-      };
+    };
     
+
+    const handleSortChange = (newSortBy) => {
+        // Update sort criteria based on pre-defined options
+        console.log(newSortBy);
+        switch (newSortBy) {
+            case 'lastUpdated':
+                setSortBy('updatedAt');
+                setSortDir('desc');
+                break;
+            // case 'mostArticles':
+            //     setSortBy('numberOfArticles');
+            //     setSortDir('desc');
+            //     break;
+            case 'createdFirst':
+                setSortBy('createdAt');
+                setSortDir('asc');
+                break;
+            case 'createdLast':
+                setSortBy('createdAt');
+                setSortDir('desc');
+                break;
+            default:
+                break;
+        }
+    };
+
+
     if (blogsLoading) {
         return <p>Loading blogs...</p>;
     }
@@ -55,9 +85,28 @@ const BlogsListPage = () => {
     return (
         <>
             <h1>Blogs</h1>
+
             <button onClick={() => {
                 navigate(`/create-blog`);
             }}>Create Blog</button>
+
+            <br/>
+
+            <label htmlFor="sortBy">Sort By: </label>
+            <select
+                id="sortBy"
+                value={
+                    sortBy === 'createdAt' ? 'createdFirst' :
+                    sortBy === 'updatedAt' ? 'lastUpdated' : ''
+                  }
+                onChange={(e) => handleSortChange(e.target.value)}
+            >
+            <option value="createdFirst">Created First</option>
+            <option value="createdLast">Newest First</option>
+            <option value="lastUpdated">Last Updated</option>
+            {/* <option value="mostArticles">Most Articles</option> */}
+            </select>
+            
             <BlogsList blogs={blogs} />
             <Pagination
                 pagination={pagination}
